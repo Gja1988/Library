15f6d5f123c8a9e1b04a35072c035d97afc2fa4a
Guillermo <garauz1988@gmail.com>
2023-07-09 17:55:10 -0400

    Fixed functions and HTML

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 let myLibrary = [];
 
+
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 };
 
-function addBookToLibrary() {
+function addBookToLibrary(event) {
 
+    event.preventDefault();
 
+    const book = document.querySelector('#book').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+
+    const newBook = new Book(book, author, pages);
+
+    myLibrary.push(newBook);
+
+    console.log(myLibrary);
 
 };
 
-console.log(addBookToLibrary());
+
+
+// bookName.addEventListener('input', addBookToLibrary);
+// authorName.addEventListener('input', addBookToLibrary);
+// pageNum.addEventListener('input', addBookToLibrary);
