131d88ed5e8b1373f8008511fe9fa7a70fcecfb4
Guillermo <garauz1988@gmail.com>
2023-07-09 22:23:41 -0400

    Create functions and buttons to show form. More styling and HTML

--- app.js
+++ app.js
@@ -21,7 +21,41 @@ function addBookToLibrary(event) {
 
     console.log(myLibrary);
 
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card');
+
+    bookCard.innerHTML = `
+    <h2>${newBook.title}</h2>
+    <p>Author: ${newBook.author}</p>
+    <p>Pages: ${newBook.pages}</p>`;
+
+    const bookCardContainer = document.querySelector('#bookCard');
+    bookCardContainer.appendChild(bookCard);
+
+    document.querySelector('#book').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+
+    const form = document.querySelector('.main');
+    form.style.display = 'none';
+
+    const button = document.querySelector('#addBook');
+    button.style.display = 'block';
+
 };
+
+const openForm = () => {
+    const form = document.querySelector('.main');
+    const button = document.querySelector('#addBook');
+
+    form.style.display = 'block';
+    button.style.display = 'none';
+}
+
+const bookCard = (book) => {
+    addBookToLibrary();
+    card.appendChild()
+}
 
 
 
